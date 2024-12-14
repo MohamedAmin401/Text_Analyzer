77181e31cf259ac482c46b82ba6b949a50790e82
Mohamed Amin <a8971377@gmail.com>
2024-12-14 18:37:41 +0200

    edit on project file

--- project.fs
+++ project.fs
@@ -1,25 +1,9 @@
-module project.fs
-
+module Program.fs
 open System
 open System.IO
-open System.Text.RegularExpressions
+open System.Windows.Forms
 
-// load the text from file
-let loadText (filePath: string) : string =
-    try
-        File.ReadAllText(filePath)
-    with
-    | :? FileNotFoundException -> 
-        printfn "File not found: %s" filePath
-        ""
-    | :? UnauthorizedAccessException ->
-        printfn "Access denied to file: %s" filePath
-        ""
-    | ex ->
-        printfn "An error occurred: %s" ex.Message
-        ""
-
-// compute the number of words 
-let countWords (text: string) : int =
-    text.Split([|' '; '\n'; '\t'; ','; '.'|], StringSplitOptions.RemoveEmptyEntries)
+// Function to count words
+let countWords (text: string) =
+    text.Split([|' '; '\t'; '\n'; '\r'; '.'; ','; '!'|], StringSplitOptions.RemoveEmptyEntries)
     |> Array.length
